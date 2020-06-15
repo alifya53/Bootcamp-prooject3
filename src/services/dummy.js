641cb7d3ea7e396efb2aa5e44e7ea4a933c7a271
Alifya Hussain <alifyahussain53@gmail.com>
2020-06-16 03:42:23 +0500

    Updated rates

--- src/services/dummy.js
+++ src/services/dummy.js
@@ -2,61 +2,61 @@ export const dummyProducts = [
   {
     id: 1,
     name: "IPhone - 5S",
-    price: 223.00,
+    price: "223.00",
     photo: "/img/1.jpg",
   },
   {
     id: 2,
     name: "Iphone - 6S Plus",
-    price: 484.2,
+    price: "484.2",
     photo: "/img/2.jpg"
   },
   {
     id: 3,
     name: "Iphone - 7S",
-    price: 547.01,
+    price: "547.01",
     photo: "/img/3.jpg"
   },
   {
     id: 4,
-    name: "Cabbage - Nappa",
-    price: 250.90,
+    name: "Iphone - X",
+    price: "1050.90",
     photo: "/img/4.jpg"
   },
   {
     id: 5,
-    name: "Sping Loaded Cup Dispenser",
-    price: 175.85,
+    name: "Huwawei - Y9 Prime",
+    price: "175.85",
     photo: "/img/5.jpg"
   },
   {
     id: 6,
-    name: "Bread - Malt",
-    price: 82.61,
+    name: "Huwawei - Y19Lite",
+    price: "182.61",
     photo: "/img/6.jpg"
   },
   {
     id: 7,
-    name: "Glass Clear 8 Oz",
-    price: 201.10,
+    name: "Huwawei- Honor9",
+    price: "201.10",
     photo: "/img/7.jpg"
   },
   {
     id: 8,
-    name: "Sour Puss Raspberry",
-    price: 134.99,
+    name: "Samsung - Note9",
+    price: "1040.99",
     photo: "/img/8.jpg"
   },
   {
     id: 9,
-    name: "Pork - Chop, Frenched",
-    price: 199.16,
+    name: "Samsung - S10",
+    price: "1099.16",
     photo: "/img/9.jpg"
   },
   {
     id: 10,
-    name: "Bagels Poppyseed",
-    price: "123.82",
+    name: "Xiomi Redme",
+    price: "83.82",
     photo: "/img/10.jpg"
   },
 ];
